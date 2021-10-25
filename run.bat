18fadb70970fcd1cb2629524e1b7928e8596dba9
NatalieOshana <90546076+NatalieOshana@users.noreply.github.com>
2021-10-25 20:35:25 +1100

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,10 @@
+set FN_AUTH_REDIRECT_URI=http://localhost:8040/google/auth
+set FN_BASE_URI=http://localhost:8040
+set FN_CLIENT_ID=691499918272-79hnmhfp1vm384tiksang7dm15d5v4p8.apps.googleusercontent.com
+set FN_CLIENT_SECRET=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
+
+set FLASK_APP=app.py
+set FLASK_DEBUG=1
+set FN_FLASK_SECRET_KEY=3546578676746353552525364657586887979702827374
+
+python -m flask run -p 8040
